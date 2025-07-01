7fee2202cfd3dd1eb6b0e745c306a0f8f85ae704
Sereyvatana <sereyvatanau@gmail.com>
2025-07-01 22:11:33 +0700

    refactor: modify migrations

--- database/migrations/0001_01_01_000000_create_users_table.php
+++ database/migrations/0001_01_01_000000_create_users_table.php
@@ -30,9 +30,14 @@ return new class extends Migration
         });
 
         Schema::create('password_reset_tokens', function (Blueprint $table) {
-            $table->string('email')->primary();
+            $table->string('email')->index();
             $table->string('token');
+            $table->string('otp', 6);
             $table->timestamp('created_at')->nullable();
+            $table->timestamp('expires_at')->nullable();
+            $table->boolean('is_used')->default(false);
+            
+            $table->primary('email');
         });
 
         Schema::create('sessions', function (Blueprint $table) {
